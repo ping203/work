50d6785bb6f5346e9179872b55e35377f7de2f80
linyngfly <linyngfly@163.com>
2018-02-26 23:00:15 +0800

    删除无用文件

--- doc/sql/tbl_admin_role.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `tbl_admin_role` VALUES (1, '游客', '游客只允许查看仪表盘', '1,2', 1);
-INSERT INTO `tbl_admin_role` VALUES (2, '管理员', '管理日常事务', '1,2,3,4,5,18,21', 1);
-INSERT INTO `tbl_admin_role` VALUES (3, '游戏策划', '主要是进行游戏管理和统计查询', '1,2,3,4,18', 1);
-INSERT INTO `tbl_admin_role` VALUES (4, '游戏测试', '游戏测试人员及主管', '1,4,18,21', 1);
-INSERT INTO `tbl_admin_role` VALUES (5, '运营方', '查询后台数据', '1,2,3', 1);
