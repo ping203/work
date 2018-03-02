058a6d1513b0528ba22aede73c6e9c2d0a21dcd3
linyang <119745743@qq.com>
2018-03-02 18:01:58 +0800

    工作同步

--- config/data/china/shop_itemtype_const_cfg.js
+++
@@ -1,9 +0,0 @@
-var	shop_itemtype_const_cfg =  {
-		IT_PEARL : 0,    //--商品钻石
-		IT_GOLD : 1,    //--商品金币
-		IT_VIP : 2,    //--商品VIP
-		IT_CARD : 3,    //--商品月卡
-		IT_GIFT : 4,    //--商品礼包
-		IT_FUND : 5,    //--翻盘基金
-	};
-module.exports = shop_itemtype_const_cfg;
